cef12a30f1aecac97db8ff3a92e6c1407c4db5fc
EmJey <emjeydev@gmail.com>
2023-06-19 09:56:47 +0330

    resolve B.M.M problem

--- 01 - Quera Algorithm Basics/Java/22 - B.M.M/BMM/src/Main.java
+++ 01 - Quera Algorithm Basics/Java/22 - B.M.M/BMM/src/Main.java
@@ -1,14 +1,47 @@
 import java.util.Scanner;
 
+// This file is made by EmJey
+// Project: B.M.M - Algorithm Basics Quera
+// FileName: Main
+// Date: 2023/06/19
+// Email: emjeydev@gmail.com
+// Github: emjeydev
+
 public class Main {
     public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//
-//        int a = sc.nextInt();
-        System.out.println(12 % 6);
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int answer = 1;
+
+        for (int i = 0; i < n; i++) {
+            if (gcd(i, n) == 1)
+                answer =  lcm(answer, i);
+        }
+
+        System.out.println(answer);
     }
 
-//    static int gcd(int a, int b) {
-//
-//    }
+    public static int gcd(int a, int b) {
+        int c = 0;
+        if (a > b) {
+            c = a;
+            a = b;
+            b = c;
+        }
+
+        while (a > 0) {
+            b = b % a;
+            c = a;
+            a = b;
+            b = c;
+        }
+
+        return b;
+    }
+
+    public static int lcm(int a, int b) {
+        return ((a * b) / (gcd(a, b)));
+    }
+
 }
