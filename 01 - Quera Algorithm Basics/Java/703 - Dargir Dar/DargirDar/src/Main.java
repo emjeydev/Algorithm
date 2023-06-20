febe3d59125070b29593b940c1842744ab6fabd6
EmJey <emjeydev@gmail.com>
2023-06-20 10:21:00 +0330

    add and solve DargirDar problem

---
+++ 01 - Quera Algorithm Basics/Java/703 - Dargir Dar/DargirDar/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+// This file is made by EmJey
+// Project: DargirDar
+// FileName: Main
+// Date: 2023/06/20
+// Email: emjeydev@gmail.com
+// Github: emjeydeva
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int doorWidth = sc.nextInt();
+        int doorHeight = sc.nextInt();
+        int masoudWidth = sc.nextInt();
+        int masoudHeight = sc.nextInt();
+
+        if (doorWidth >= masoudWidth && doorHeight >= masoudHeight)
+            System.out.println("yes");
+        else
+            System.out.println("no");
+    }
+}
