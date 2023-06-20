a6ea35d54dc4dca9db1901df1fa125c54f88d456
EmJey <emjeydev@gmail.com>
2023-06-20 10:48:23 +0330

    add GhatarKamyabi project

---
+++ 01 - Quera Algorithm Basics/Java/704 - Ghatar Kamyabi/GhatarKamyabi/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+// This file is made by EmJey
+// Project: GhatarKamyabi
+// FileName: Main
+// Date: 2023/06/20
+// Email: emjeydev@gmail.com
+// Github: emjeydev
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int k = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+    }
+}
