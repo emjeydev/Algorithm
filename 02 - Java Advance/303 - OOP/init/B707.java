ea1bc086d26504253e086d196f30d4b428b32458
EmJeyDev <emjey@dev.com>
2025-05-05 15:08:42 +0330

    fix runtime error in the third exercise

--- 02 - Java Advance/303 - OOP/init/B707.java
+++ 02 - Java Advance/303 - OOP/init/B707.java
@@ -1,4 +1,4 @@
-public class B707 extends Airplane{
+public class B707 extends Airplane {
     public B707(String name, int price, int numberOfSeats, int maxSpeed, String fuel, int numberOfFins, int numberOfWheels, String steeringWheel, String airline, int numberOfCrew, String captain) {
         super(name, price, numberOfSeats, maxSpeed, fuel, numberOfFins, numberOfWheels, steeringWheel, airline, numberOfCrew, captain);
     }
--- 02 - Java Advance/303 - OOP/init/Vehicle.java
+++ 02 - Java Advance/303 - OOP/init/Vehicle.java
@@ -1,45 +1,13 @@
 public class Vehicle {
-    private String name;
-    private int price;
-    private int numberOfSeats;
-    private int maxSpeed;
+    String name;
+    int price;
+    int numberOfSeats;
+    int maxSpeed;
 
-    public Vehicle(String nam1e, int price, int numberOfSeats, int maxSpeed) {
-        this.name = nam1e;
-        this.price = price;
-        this.numberOfSeats = numberOfSeats;
-        this.maxSpeed = maxSpeed;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
+    public Vehicle(String name, int price, int numberOfSeats, int maxSpeed) {
         this.name = name;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
         this.price = price;
-    }
-
-    public int getNumberOfSeats() {
-        return numberOfSeats;
-    }
-
-    public void setNumberOfSeats(int numberOfSeats) {
         this.numberOfSeats = numberOfSeats;
-    }
-
-    public int getMaxSpeed() {
-        return maxSpeed;
-    }
-
-    public void setMaxSpeed(int maxSpeed) {
         this.maxSpeed = maxSpeed;
     }
 }
